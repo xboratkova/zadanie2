d7cb5be1db5ae229e557cb8159c25b041e1c1e46
Kristina Boratkova <k.boratkova@gmail.com>
2020-06-25 15:51:49 +0200

    procedura na LU rozklad

--- 2_zadanie_kboratkova.c
+++ 2_zadanie_kboratkova.c
@@ -9,4 +9,34 @@ typedef struct{
 
 #define ELEM(A,r,c) (A.elem[(A.cols)*r+c])
 
+void LUdecomposition(MAT *a, MAT *l, MAT *u, int n) {
+	int i,j,k;
+	for(i = 0; i < n; i++){
+		for(j = 0; j < n; j++){
+			if(j < i)
+				ELEM(l,j,i) = 0;
+			else {
+				ELEM(l,j,i) = ELEM(a,j,i);
+				for(k = 0; k < i; k++){
+					ELEM(l,j,i) = ELEM(l,j,i) - ELEM(l,j,k) * ELEM(u,k,i);
+				}
+			}
+		}
+		for(j = 0; j < n; j++){
+			if(j < i) 
+				ELEM(u,i,j) = 0;
+			else if (j == i)
+					ELEM(u,i,j) = 1;
+				else{
+					ELEM(u,i,j) = ELEM(a,i,j) / ELEM(l,i,i);
+					for(k = 0; k < i; k++){
+						ELEM(u,i,j) = ELEM(u,i,j) - ((ELEM(l,i,k) * ELEM(u,k,j) / ELEM(l,i,i)));
+					}
+					
+				}
+				
+			}
+		}
+	}
+	
 
