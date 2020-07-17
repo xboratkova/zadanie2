52f5917b4e067233e42d06dd4510792ecf227731
Kristina Boratkova <k.boratkova@gmail.com>
2020-07-17 13:00:25 +0200

    odstranenie zbytocneho argumentu z fukcii

--- 2_zadanie_kboratkova.c
+++ 2_zadanie_kboratkova.c
@@ -10,10 +10,10 @@ typedef struct{
 
 #define ELEM(A,r,c) (A->elem[(A->cols)*c+r])
 
-char mat_LU_decompose(MAT *a, MAT *l, MAT *u, int n) {
+char mat_LU_decompose(MAT *a, MAT *l, MAT *u) {
 	int i,j,k;
-	for(i = 0; i < n; i++){
-		for(j = 0; j < n; j++){
+	for(i = 0; i < a->cols; i++){
+		for(j = 0; j < a->rows; j++){
 			if(j < i)
 				ELEM(l,j,i) = 0;
 			else {
@@ -23,7 +23,7 @@ char mat_LU_decompose(MAT *a, MAT *l, MAT *u, int n) {
 				}
 			}
 		}
-		for(j = 0; j < n; j++){
+		for(j = 0; j < a->rows; j++){
 			if(j < i) 
 				ELEM(u,i,j) = 0;
 			else if (j == i)
@@ -40,21 +40,21 @@ char mat_LU_decompose(MAT *a, MAT *l, MAT *u, int n) {
 		}
 	}
 	
-void nasobenie(MAT *l, MAT *u, MAT *nasobok, int n){
+void nasobenie(MAT *l, MAT *u, MAT *nasobok){
 	int i,j,k;
-	for(i = 0; i < n; i++){
-		for(j = 0; j < n; j++){
-			for(k = 0; k < n; k++){
+	for(i = 0; i < l->rows; i++){
+		for(j = 0; j < l->cols; j++){
+			for(k = 0; k < l->rows; k++){
 				ELEM(nasobok, i, j) += ELEM(l, i, k)*ELEM(u, k ,j);
 			}
 		}
 	}
 }
 
-int skuska(MAT *a, MAT *nasobok, int n){
+int skuska(MAT *a, MAT *nasobok){
 	int i,j;
-	for(i = 0; i < n; i++){
-		for(j = 0; j < n; j++){
+	for(i = 0; i < a->rows; i++){
+		for(j = 0; j < a->cols; j++){
 			if(ELEM(a,i,j) != ELEM(nasobok,i,j)){
 				return 0;
 			}
@@ -63,31 +63,31 @@ int skuska(MAT *a, MAT *nasobok, int n){
 	return 1;
 }
 	
-void mat_unit(MAT *mat, int n){
+void mat_unit(MAT *mat){
 	int i,j;
-	for(i=0; i<n; i++){
-		for(j=0; j<n; j++){
+	for(i=0; i< mat->rows; i++){
+		for(j=0; j< mat->cols; j++){
 			if(i==j) ELEM(mat, i, j)=1;
 			else ELEM(mat, i ,j)=0;
 		}
 	}
 }
-void mat_random(MAT *mat, int n){
+void mat_random(MAT *mat){
 	int i,j;
 	
 	
-	for(i=0; i<n; i++){
-		for(j=0; j<n; j++){
+	for(i=0; i< mat->cols; i++){
+		for(j=0; j< mat->rows; j++){
 			ELEM(mat, i, j)= -1+(1-(-1))*((float)rand()/(float)(RAND_MAX));
 		}
 	}
 }
 
-void mat_print(MAT *mat, int n){
+void mat_print(MAT *mat){
 	int i, j;
 	
-	for(i=0; i<n; i++){
-		for(j=0; j<n; j++){
+	for(i=0; i< mat->rows; i++){
+		for(j=0; j< mat->cols; j++){
 			if(ELEM(mat,i,j)<0) printf(" %.2f", ELEM(mat, i, j));
 			else printf("  %.2f", ELEM(mat, i, j));
 		}
@@ -119,18 +119,18 @@ void mat_print(MAT *mat, int n){
 		l.elem = pole2;
 		
 		
-		mat_random(&a,n);
+		mat_random(&a);
 			
 		printf("Matica A:\n");
-		mat_print(&a, n);
+		mat_print(&a);
 		
-		mat_LU_decompose(&a, &l, &u, n);
+		mat_LU_decompose(&a, &l, &u);
 			
 		printf("Matica L:\n");
-		mat_print(&l,n);
+		mat_print(&l);
 		
 		printf("Matica U:\n");
-		mat_print(&u,n);
+		mat_print(&u);
 		
 		MAT nasobok;
 		nasobok.cols = n;
@@ -138,12 +138,12 @@ void mat_print(MAT *mat, int n){
 		float pole3[n*n];
 		nasobok.elem = pole3;
 		
-		nasobenie(&l, &u, &nasobok, n);
+		nasobenie(&l, &u, &nasobok);
 		
 		printf("Nasobenie L a U:\n");
-		mat_print(&nasobok, n);
+		mat_print(&nasobok);
 		
-		if(skuska(&a, &nasobok, n)==1)
+		if(skuska(&a, &nasobok)==1)
 			printf("Nasobok LU je zhodny s maticou A\n");
 		else 
 			printf("Nasbok LU nie je zhodny s maticou A, niekde nastala chyba\n");
